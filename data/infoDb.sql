13e160ba90261950825d07763eda700574e88e17
BESSARD Lucas <bessardl@hostname.iut2.upmf-grenoble.fr>
2018-10-22 17:52:43 +0200

    BD DEPART V6

--- data/infoDb.sql
+++ data/infoDb.sql
@@ -6,7 +6,6 @@ prix FLOAT,
 region TEXT,
 caracteristique TEXT,
 id INTEGER,
-miseligne DATE,
 FOREIGN KEY id REFERENCE utilisateur(id),
 FOREIGN KEY region REFERENCE utilisateur(region)
 };
@@ -18,7 +17,6 @@ prenomUser TEXT,
 AGE INTEGER,
 mdp TEXT,
 region TEXT,
-TYPE voffreperso is VARRAY(10) OF INTEGER,
 TYPE vOfrreSuivi is VARRAY(10) OF INTEGER,
 mail TEXT,
 TELEPHONE INTEGER
