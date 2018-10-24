4188ed6f8dc35403dc52e3f47cd0017ee4ea564e
PRAT-CAPILLA Hugo <pratcaph@hostname.iut2.upmf-grenoble.fr>
2018-10-24 09:39:23 +0200

    Preparation de l'affichage 2

---
+++ controleur/mainPage.controleur.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once('../model/model.class.php');
+    require_once('../model/model.dao.php');
+
+    $config = parse_ini_file('../config/config.ini');
+
+
+    //$jukebox = new MusicDAO($config['database_path']);
+
+    $nbElemPage = 10;
+
+    $nbMusic = $jukebox->getnbMusic();
+
+    $nextId = (isset($_GET['firstId']) && $_GET['firstId'] != 1? (($_GET['firstId']+10) >= $nbMusic ? 1: $_GET['firstId']+10): 10);
+    $precId = (isset($_GET['firstId']) && $_GET['firstId'] != 1? $_GET['firstId']-10: $nbMusic-($nbMusic%10));
+    $precId = ($precId == 0 ? 1 : $precId);
+
+    $noPage = ($nextId == 1 ? ($nbMusic-($nbMusic%10))/10 : $nextId/10);
+
+    $firstId = (isset($_GET['firstId'])? $_GET['firstId']: 1);
+    $lastId = (($firstId+10) > $nbMusic? $nbMusic : $firstId+10);
+
+    for($i=$firstId; $i<$lastId;$i++){
+      // Récupération de l'objet Music
+      $m = $jukebox->get($i);
+      // Ajout à la liste des images à afficher
+      $list[$i] = $config['data_url'].'/img/'.$m->cover;
+    }
+
+    include('../view/main.view.php');
+
+?>
