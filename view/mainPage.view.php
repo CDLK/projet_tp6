c15814801a0556607a127736f0b0054a17acedbb
PRAT-CAPILLA Hugo <pratcaph@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:40:32 +0200

    debut view css

--- view/mainPage.view.php
+++ view/mainPage.view.php
@@ -2,13 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style/mainPage.style.css" />
     <title>Les Halles du Web</title>
   </head>
   <body>
     <header>
-
+      <h1>Les halles du web !</h1>
     </header>
+    <nav>
+      
+    </nav>
 
     <footer></footer>
   </body>
