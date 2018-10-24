efc3be78cc3c4ff5fc9c88c7148daaa3cc93a790
PRAT-CAPILLA Hugo <pratcaph@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:45:50 +0200

    BD + view

--- view/mainPage.view.php
+++ view/mainPage.view.php
@@ -10,9 +10,12 @@
       <h1>Les halles du web !</h1>
     </header>
     <nav>
-      
+      <?php foreach ($categorie as $categories => $value) { // Affichage de chaque categorie?>
+        <div class="Categorie">
+            <h4><?=echo $categorie->intitule?></h4>
+        </div>
+      <?php  }?>
     </nav>
-
     <footer></footer>
   </body>
 </html>
