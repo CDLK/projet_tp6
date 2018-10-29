042722c2dc07bef8d2a8242cfcf33efe47a60f86
CDLK <popoto38697@gmail.com>
2018-10-29 14:48:11 +0100

    Rattrapage css

--- view/mainPage.pres.php
+++ view/mainPage.pres.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="mainPage.style.css">
+    <link rel="stylesheet" href="style/mainPage.style.css">
     <title>Test Les Halles du Web</title>
   </head>
   <body>
